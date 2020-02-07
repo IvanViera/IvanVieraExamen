02c1ac7dbbdaa49c1c023131c2f379e1461c3f5d
IvanViera <58507049+IvanViera@users.noreply.github.com>
2020-02-07 12:50:18 +0000

    Add files via upload

--- main.php
+++ main.php
@@ -1,35 +1,31 @@
 
 
 <?php
-    
- /* COMPLETE THIS FILE WITH PHP */
-   
-    if (isset($_GET['dni'])){
+  
 
-    	$sentece= $_GET['sentence'];
+        $sentence = $_GET['sentence'];
         $servername = "localhost:3307";
         $username = "root";
-        $password = "";
+        $password = "1234";
         $dbname = "examen";
-    
+        
         $conn = new mysqli($servername, $username, $password, $dbname);
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
-        } 
-        
-    
-        $sql = "SELECT sentence FROM refranero WHERE sentence LIKE '[info a buscar]%';";
-        
-        $result = $conn->query($sql);
-            if ($result->num_rows > 0) {
-                while($row = $result->fetch_assoc()) {
-                	echo $row['sentence'];
-                }
-            } 
-            else {
-                echo "0 results";
-            }
-        $conn->close();
+            #return false;
+        }
 
-    }  
+        
+        $sql = 'SELECT sentence FROM refranero WHERE sentence LIKE "'.$sentence.'%"';
+        $result = $conn->query($sql);
+    
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                echo $row["sentence"];
+            }
+        } else {
+            echo "0 results";
+        }
+        $conn->close();
+    #}
 ?>
